5f7e82e2613fd1f52cfa077b06fcd8a10e0b36f7
bennu7 <laluibnuhidayatullah@gmail.com>
2022-11-14 00:54:08 +0700

    import assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
+import 'package:bookingapp_bloc/data/src/img_string.dart';
 import 'package:bookingapp_bloc/presentation/widget/button/primary_button.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'config/config.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,13 +12,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        // fontFamily: GoogleFonts.poppins().fontFamily,
+        fontFamily: 'Poppins',
+      ),
       home: Scaffold(
-        body: Center(
-          child: PrimaryButton(
-            onPressed: () {},
-            width: 120,
-            type: PrimaryButtonType.type3,
-          ),
+        appBar: AppBar(
+          title: const Text('Booking App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: PrimaryButton(
+                onPressed: () {},
+                width: 120,
+                type: PrimaryButtonType.type3,
+              ),
+            ),
+            Image.asset(ImgString.abstract0),
+          ],
         ),
       ),
     );
